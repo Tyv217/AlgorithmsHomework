6c4a49883ce6643a90bd61cb31cc387e938d7475
Thomas <thomasyuan1@gmail.com>
2021-02-19 22:00:11 +0800

    BinomialHeapNode

--- src/BinomialHeapNode.java
+++ src/BinomialHeapNode.java
@@ -1,4 +1,8 @@
 public class BinomialHeapNode<T> {
     public int key;
     public T data;
+    public BinomialHeapNode(int key, T data){
+        this.key = key;
+        this.data = data;
+    }
 }
